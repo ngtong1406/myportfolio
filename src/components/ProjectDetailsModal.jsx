65c6e4b86fd55d0b2adccfa6562cf0251fe5de70
Nguyen Tong Tran <ngtong1406@gmail.com>
2026-09-11 01:41:51 +0930

    Add default pages and component scode

---
+++ src/components/ProjectDetailsModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectDetailsModal = () => {
+    return <div></div>;
+};
+
+export default ProjectDetailsModal;
---
+++ src/pages/ProjectsSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectsSection = () => {
+    return <div></div>;
+};
+
+export default ProjectsSection;
