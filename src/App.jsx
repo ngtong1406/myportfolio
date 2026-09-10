9d7094e0f64cd06a9405fdcbbfec28ef22fbe1f1
Nguyen Tong Tran <ngtong1406@gmail.com>
2026-09-10 18:00:16 +0930

    Remove all older components to prepare for new design changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import BackgroundPage from "./pages/BackgroundPage";
-import ContactPage from "./pages/ContactPage";
-import HomePage from "./pages/HomePage";
-import ProjectsPage from "./pages/ProjectsPage";
-import ResumePage from "./pages/ResumePage";
-import FooterPage from "./pages/FooterPage";
-
 const App = () => {
-	const [contactPopupShown, setContactPopupShown] = useState(false);
-
-	return (
-		<div className="h-full w-full app-bg-gradient">
-			<NavBar setContactPopupShown={setContactPopupShown} />
-			<HomePage />
-			<BackgroundPage />
-			<ProjectsPage />
-			<ResumePage />
-			<FooterPage />
-			<ContactPage
-				contactPopupShown={contactPopupShown}
-				setContactPopupShown={setContactPopupShown}
-			/>
-		</div>
-	);
+    return <div className=""></div>;
 };
 
 export default App;
